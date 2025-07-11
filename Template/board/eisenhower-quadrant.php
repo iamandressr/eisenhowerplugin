99a3239dd9856b0884455f7dde0aaf02662590c3
Andres Silva Roa <andres.silvar@dgac.gob.cl>
2025-07-11 13:47:52 -0400

    cambios en las carpetas

--- Template/board/eisenhower-quadrant.php
+++ Template/board/eisenhower-quadrant.php
@@ -3,3 +3,7 @@
 <div style="background: yellow; padding: 20px;">
   ¡Cuadrante Eisenhower funcionando!
 </div>
+
+<script>
+  console.log('✅ Plugin Eisenhower renderizado correctamente');
+</script>
