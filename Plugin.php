75139bb60c2bc2e9d140dbb38def575014d9111f
Andres Silva Roa <andres.silvar@dgac.gob.cl>
2025-07-11 11:16:55 -0400

    Se modifica plugin.php

--- Plugin.php
+++ Plugin.php
@@ -12,7 +12,7 @@ class Plugin extends Base
     $this->route->addRoute('eisenhower', 'EisenhowerController', 'show', 'project');
 
     // Inserta el cuadrante Eisenhower después de las columnas del tablero
-    $this->template->hook->attach('template:board:show:after-columns', 'eisenhower:board/eisenhower-quadrant');
+    $this->template->hook->attach('template:board:show:after-columns', 'eisenhower:template/board/eisenhower-quadrant');
 
     // Carga el CSS del plugin
     $this->hook->on('template:layout:css', ['plugins/Eisenhower/Assets/css/eisenhower.css']);
